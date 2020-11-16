41a4816b9f3709987ee922f1bb104eccdb233f35
zhangjp@itnova.com.cn <zhangjp@itnova.com.cn>
2020-11-16 16:58:35 +0800

    bbb

--- blog/service/app/router/default.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line strict
-module.exports = app => {
-  // eslint-disable-next-line object-curly-spacing
-  const {router, controller} = app;
-  router.get('/default/index', controller.default.home.index);
-  router.get('/default/getArticleList', controller.default.home.getArticleList);
-  router.get('/default/getArticleById/:id', controller.default.home.getArticleById);
-};
