ec774e8bdbee53a481ca187863443906a9a4e4e1
zhangjp@itnova.com.cn <zhangjp@itnova.com.cn>
2020-11-18 10:45:23 +0800

    后台添加

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./pages/Main";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById("root"));
